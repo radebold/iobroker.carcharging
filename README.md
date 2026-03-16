9238e23505f6f8e38e784a4670bc41588f8370c2
Thomas <radebold@gmail.com>
2026-03-16 12:13:26 +0100

    0108a

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# ioBroker CarCharging
-
-Version: 0.1.0-alpha.7
-
-## Included in alpha.7
-
-- multi-vehicle config and runtime states
-- TomTom distance service
-- station service
-- nearest charging station per vehicle
-- widget JSON data under `widgets.vehicleCards`
-- compatibility mirror for first enabled vehicle under `car.*`
-- `tools.refreshNow`, `tools.lastRefresh`, `tools.lastRefreshResult`
+# iobroker.cpt
+Test Test Chargepoint Chargingg station
--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -1,10 +1,17 @@
-
 const fs = require('fs');
-const pkg = JSON.parse(fs.readFileSync('./package.json'));
-const ioPkg = JSON.parse(fs.readFileSync('./io-package.json'));
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const ioPkg = JSON.parse(fs.readFileSync('./io-package.json', 'utf8'));
+const lock = JSON.parse(fs.readFileSync('./package-lock.json', 'utf8'));
+
+ioPkg.common = ioPkg.common || {};
 ioPkg.common.version = pkg.version;
-ioPkg.native.version = pkg.version;
-ioPkg.version = pkg.version;
-fs.writeFileSync('./io-package.json', JSON.stringify(ioPkg, null, 2) + '
-');
+lock.version = pkg.version;
+if (lock.packages && lock.packages['']) {
+  lock.packages[''].version = pkg.version;
+}
+
+fs.writeFileSync('./io-package.json', JSON.stringify(ioPkg, null, 2) + '\n');
+fs.writeFileSync('./package-lock.json', JSON.stringify(lock, null, 2) + '\n');
+
 console.log(`Version synchronized: ${pkg.version}`);
